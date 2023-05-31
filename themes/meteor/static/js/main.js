291f45663c9788a611ac5b24e60120928ca1178d
lovezsh <shuxhan@163.com>
2023-05-31 15:52:22 +0800

    Add files via upload

---
+++ themes/meteor/static/js/main.js
@@ -0,0 +1,62 @@
+$(function () {
+  $('.post-content a').attr('target', '_blank');
+  $('#TableOfContents a').attr('target', '')
+  $('.post-content img').addClass('slb');
+  $('.slb').simplebox({
+    fadeSpeed: 100
+  });
+  $('iframe').wrap('<p class="iframe"></p>');
+
+  $(document).on('scroll', function () {
+    var $pageScrollTop = $(this).scrollTop()
+    if ($pageScrollTop > 200) {
+      $('.gotop').addClass('act')
+    } else {
+      $('.gotop').removeClass('act')
+    }
+  })
+
+  $('.gotop').on('click', function () {
+    $('html, body').stop().animate({
+      scrollTop: 0
+    })
+  })
+
+  $('.mob-menu').click(function () {
+    $(this).toggleClass('act');
+    $('.nav').toggleClass('active');
+  })
+
+  $('.nav a').each(function () {
+    if ($(this).attr('href') == window.location.pathname) {
+      $(this).addClass('active');
+    }
+  })
+})
+
+// 统计总数字和文章数
+$(document).ready(function () {
+  $.ajax({
+    url: '/index.json',
+    type: 'get',
+    dataType: 'json',
+    success: function (data) {
+      const pageUrls = data;
+      const totalNum = pageUrls.length;
+      $('#totalNum').html(totalNum);
+      let totalWords = 0;
+      pageUrls.forEach(urlObj => {
+        $.get(urlObj.permalink, function (data) {
+          const content = data.replace(/(<([^>]+)>)/gi, " ").replace(/[^\w\s]/gi, " ");
+          const words = content.split(" ");
+          const wordCount = words.filter(word => word !== "").length;
+          totalWords += wordCount;
+          $('#totalWords').html(totalWords);
+        });
+      });
+    },
+    error: function () {
+      console.log('error')
+    }
+  })
+});
