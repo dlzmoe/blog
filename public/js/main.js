9e77e5bb51990eb7bdf871232fce35e947c49b81
lovezsh <shuxhan@163.com>
2023-07-06 14:06:38 +0800

    备份

--- public/js/main.js
+++ public/js/main.js
@@ -72,6 +72,22 @@ function isMob() {
 
 $("#searchTerm").focus();
 
-$('.rss').click(function(){
+$('.rss').click(function () {
   window.location.href = '/index.xml'
 })
+
+$(window).off('scroll');
+let progress_bar = $('.progress-bar');
+let win_h = $(window).height();
+let body_h = $('body').height();
+let sHeight = body_h - win_h;
+
+function updateProgress(p) {
+  progress_bar.css('width', p * 100 + '%');
+}
+$(window).on('scroll', function () {
+  window.requestAnimationFrame(function () {
+    let perc = Math.max(0, Math.min(1, $(window).scrollTop() / sHeight));
+    updateProgress(perc);
+  });
+});
