98f939e919551796b93470bc1819d4f9fceef8b9
lovezsh <shuxhan@163.com>
2023-07-30 23:14:41 +0800

    修改js

--- themes/meteor3/static/js/main.js
+++ themes/meteor3/static/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-  $('.post-content a').attr('target', '_blank');
+  // $('.post-content a').attr('target', '_blank');
   $('#TableOfContents a').attr('target', '')
   $('.post-content img').addClass('slb');
   $('.slb').simplebox({
