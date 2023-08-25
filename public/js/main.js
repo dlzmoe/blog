94a43f7641353979c4be9fd055feb076466ea935
zishume <shuxhan@163.com>
2023-08-25 17:17:44 +0800

    提交

---
+++ public/js/main.js
@@ -0,0 +1,57 @@
+$(function () {
+  $('.post-content img').addClass('slb');
+  $('.slb').simplebox({
+    fadeSpeed: 100
+  });
+  // $('.post-content iframe').wrap('<p class="iframe"></p>');
+
+  $(document).on('scroll', function () {
+    var $pageScrollTop = $(this).scrollTop()
+    if ($pageScrollTop > 200) {
+      $('.gotop').addClass('act')
+    } else {
+      $('.gotop').removeClass('act')
+    }
+  })
+
+  $('.gotop').on('click', function () {
+    $('html, body').stop().animate({
+      scrollTop: 0
+    })
+  })
+
+  $('nav em').click(function () {
+    $(this).toggleClass('act');
+    $('nav ul').slideToggle();
+  })
+
+  $('nav ul a').each(function () {
+    if ($(this).attr('href') == window.location.pathname) {
+      $(this).addClass('active');
+    }
+  })
+
+})
+
+$("#searchTerm").focus();
+
+// 在代码段显示对应代码语言
+// $('.markdown-body pre>code').each(function () {
+//   $(this).parents('.highlight').prepend('<span class="data-lang">' + $(this).attr('data-lang') + '</span>')
+// })
+
+function comments() {
+  $('.comments-title>li:nth-child(1)').addClass('active');
+  $('.comments-content>.item:nth-child(1)').show();
+
+  $('.comments-title>li').each(function () {
+    $(this).click(function () {
+      $('.comments-title>li').removeClass('active');
+      $(this).addClass('active');
+      var index = $(this).index() + 1;
+      $('.comments-content>.item').hide();
+      $('.comments-content>.item:nth-child(' + index + ')').show();
+    })
+  })
+}
+comments();
