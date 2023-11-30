75f9fbc624f684cfaef47b2a03ba159cbe86b2ab
98zi <anghunk@gmail.com>
2023-11-30 17:40:54 +0800

    图片灯箱

--- themes/meteor/static/js/main.js
+++ themes/meteor/static/js/main.js
@@ -1,4 +1,8 @@
 $(function () {
+  $('.post-content img').addClass('slb');
+  $('.slb').simplebox({
+    fadeSpeed: 100
+  });
   $('.post-content iframe').wrap('<div class="iframe"></div>');
 
   $('nav a').each(function () {
