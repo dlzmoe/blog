accf9e018f378e2ea4c9ff883991da02973a0e71
98zi <anghunk@gmail.com>
2023-12-21 18:01:49 +0800

    修改

--- themes/dark/static/js/main.js
+++ themes/dark/static/js/main.js
@@ -1,13 +1,15 @@
 $(function () {
-  $('.post-content img').addClass('slb');
+  $('.postbody img').addClass('slb');
   $('.slb').simplebox({
     fadeSpeed: 100
   });
-  $('.post-content iframe').wrap('<div class="iframe"></div>');
+  $('.postbody iframe').wrap('<div class="iframe"></div>');
 
   $('nav a').each(function () {
     if ($(this).attr('href') == window.location.pathname) {
       $(this).addClass('active');
     }
   })
+
+
 })
