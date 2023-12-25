fe95edd8c0c7162dee84237bd7fecfeeda6d2db5
98zi <anghunk@gmail.com>
2023-12-25 21:15:00 +0800

    灯箱

--- themes/leerob/static/js/main.js
+++ themes/leerob/static/js/main.js
@@ -1,18 +1,27 @@
 $(function () {
-  $('.content img').addClass('slb');
-  $('.slb').simplebox({
-    fadeSpeed: 100
-  });
   $('.content iframe').wrap('<div class="iframe"></div>');
-
   $('nav a').each(function () {
     if ($(this).attr('href') == window.location.pathname) {
       $(this).addClass('active');
     }
   })
 
-  // 
-  $('#menu-btn').click(function () {
-    $('.site-nav').toggleClass('act');
+  $('.content img').addClass('zimgbox');
+  $('body').append('<div id="zimgbox-wrap" style="display:none"><img src></div>');
+  $('.zimgbox').each(function () {
+    $(this).click(function () {
+      console.log('1');
+      var $url = $(this).attr('src');
+      $('html').css('overflow-y', 'hidden');
+      $('#zimgbox-wrap').css('display', 'flex');
+      $('#zimgbox-wrap img').addClass('zimgbox-act');
+      $('#zimgbox-wrap img').attr('src', $url);
+    })
+  })
+  $('#zimgbox-wrap').click(function () {
+    $('html').css('overflow-y', 'scroll');
+    $('#zimgbox-wrap').css('display', 'none');
+    $('#zimgbox-wrap img').removeClass('zimgbox-act');
+    $('#zimgbox-wrap img').attr('src', '');
   })
 })
