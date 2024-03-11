7c5286537dd4974e5986da1b9d98049ca55f571c
98zi <anghunk@gmail.com>
2024-03-11 10:30:10 +0800

    新增天数计算

--- themes/blue/static/js/main.js
+++ themes/blue/static/js/main.js
@@ -39,4 +39,18 @@ $(function () {
   //   $(this).parents('.chroma').before(`<span class="tag">${datalang}</span>`);
   // })
 
+  function TimeDifference() {
+    const time = $('.entry-meta time').html();
+    var now = new Date(); // 获取当前时间
+    var end = new Date(time); // 设置结束时间
+    var diff = Math.abs(now - end); // 计算两个日期之间的差值，结果是毫秒数
+    var diffDays = Math.ceil(diff / (1000 * 60 * 60 * 24)); // 将毫秒转换为天数
+
+    if (diffDays < 99) {
+      $('.expired-tips').hide();
+    } else {
+      $('.expired-tips').show().html(`提醒：本文最后更新于 ${diffDays} 天前，文中所描述的信息可能已发生改变，请谨慎使用。`)
+    }
+  }
+  TimeDifference();
 })
